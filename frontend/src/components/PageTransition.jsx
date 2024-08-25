51e92bcd0a73943c61f514237e30bed7bc7c43de
Timothy Attah <timothyattah733@gmail.com>
2024-08-25 01:27:34 +0100

    Change the page transition background color

--- frontend/src/components/PageTransition.jsx
+++ frontend/src/components/PageTransition.jsx
@@ -1,12 +1,12 @@
 import React, { useRef, useEffect } from 'react';
 import styled from 'styled-components';
-import { Power4} from 'gsap';
+import { Power4 } from 'gsap';
 
 export const TransitionEffect = styled.div`
   position: fixed;
   z-index: 1000000000000;
   background-color: #474242;
-  background-image: repeating-linear-gradient(
+  /* background-image: repeating-linear-gradient(
       to right,
       #2d2a44 0 1px,
       transparent 2px 200px
@@ -14,7 +14,7 @@ export const TransitionEffect = styled.div`
     repeating-linear-gradient(to left, #2d2a44 0 1px, transparent 2px 200px),
     radial-gradient(at 50% 50%, #2d2a44, #1d1b34);
   --to: top;
-  background-image: linear-gradient(to var(--to), #89a5df, #e46e7f, #e8e191);
+  background-image: linear-gradient(to var(--to), #89a5df, #e46e7f, #e8e191); */
   top: 0;
   width: 100%;
   height: 100vh;
