ea6ca1c39bc82202457e18a1cbd4a842e0f56fb5
Timothy Attah <timothyattah733@gmail.com>
2024-10-04 21:13:51 +0100

    Changed cors policy

--- server/server.js
+++ server/server.js
@@ -18,8 +18,8 @@ const corsOrigin = {
   credentials: true,
   optionSuccessStatus: 200,
 };
-// app.use(cors());
-app.use(cors(corsOrigin));
+app.use(cors());
+// app.use(cors(corsOrigin));
 
 app.get('/', (req, res) => {
   res.send('Welcome To Enoch Louis Coding Bootcamp School');
